b8c70aabe59ff68c45f1f38cdcda2dae3d7173d2
Jerry Li <jerli@xogrp.com>
2018-07-25 17:55:32 +0800

    Finished Functions showcase.

--- src/main/kotlin/com/learn/unittest/basic/func/FunctionsShowcase.kt
+++ src/main/kotlin/com/learn/unittest/basic/func/FunctionsShowcase.kt
@@ -3,11 +3,29 @@ package com.learn.unittest.basic.func
 class FunctionsShowcase {
 
     fun max(one: Int, another: Int): Int {
-        val title = "Kotlin"
         return if (one > another) one else another
     }
 
     fun maxWithSingleExpressionBody(one: Int, another: Int): Int = if (one > another) one else another
 
     fun maxWithTypeInference(one: Int, another: Int) = if (one > another) one else another
+
+    fun <T> joinToString(collection: Collection<T>, separator: String, prefix: String, postfix: String): String{
+        val result = StringBuffer(prefix)
+        for((index, element) in collection.withIndex()){
+            if(index > 0){
+                result.append(separator)
+            }
+            result.append(element)
+        }
+        result.append(postfix)
+        return result.toString()
+    }
+
+    fun <T>joinStringWithDefaultParaValue(collection: Collection<T>,
+                                          separator: String = ", ",
+                                          prefix: String = "{",
+                                          postfix: String = "}"): String{
+        return joinToString(collection, separator, prefix, postfix)
+    }
 }
--- src/test/kotlin/com/learn/unittest/basic/func/FunctionsShowcaseTest.kt
+++ src/test/kotlin/com/learn/unittest/basic/func/FunctionsShowcaseTest.kt
@@ -1,26 +1,68 @@
 package com.learn.unittest.basic.func
 
 import org.junit.Assert
-import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 
-class FunctionsShowcaseTest{
+class FunctionsShowcaseTest {
+
+    companion object {
+        private const val JOIN_STRING_EXPECTED = "{1, 2, 3}";
+    }
+
+    private lateinit var showcase: FunctionsShowcase
+
+    @Before
+    fun setUp() {
+        showcase = FunctionsShowcase()
+    }
 
     @Test
-    fun test_max(){
-        val showcase = FunctionsShowcase()
+    fun test_max() {
         Assert.assertEquals(5, showcase.max(5, 4))
     }
 
     @Test
-    fun test_max_with_single_expression_body(){
-        val showcase = FunctionsShowcase()
+    fun test_max_with_single_expression_body() {
         Assert.assertEquals(4, showcase.maxWithSingleExpressionBody(1, 4))
     }
 
     @Test
-    fun test_max_with_type_inference(){
-        val showcase = FunctionsShowcase()
+    fun test_max_with_type_inference() {
         Assert.assertEquals(6, showcase.maxWithTypeInference(6, 4))
+    }
+
+    @Test
+    fun test_join_to_string() {
+        Assert.assertEquals(JOIN_STRING_EXPECTED, showcase.joinToString(listOf(1, 2, 3), ", ", "{", "}"))
+    }
+
+    @Test
+    fun test_join_to_string_with_named_argument() {
+        val result =
+                showcase.joinToString(
+                        collection = listOf(1, 2, 3),
+                        separator = ", ",
+                        prefix = "{",
+                        postfix = "}")
+        Assert.assertEquals(JOIN_STRING_EXPECTED, result)
+    }
+
+    @Test
+    fun test_join_to_string_with_default_para_value() {
+        val result = showcase.joinStringWithDefaultParaValue(listOf(1, 2, 3))
+        Assert.assertEquals(JOIN_STRING_EXPECTED, result)
+    }
+
+    @Test
+    fun test_join_to_string_omit_trailing_arguments() {
+        val result = showcase.joinStringWithDefaultParaValue(listOf(1, 2, 3), "#")
+        Assert.assertEquals("{1#2#3}", result)
+    }
+
+    @Test
+    fun test_join_to_string_omit_arguments_with_named_arguments() {
+        val result = showcase.joinStringWithDefaultParaValue(listOf(1, 2, 3), postfix = "@")
+        Assert.assertEquals("{1, 2, 3@", result)
     }
 }
